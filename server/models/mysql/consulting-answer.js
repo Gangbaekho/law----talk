da810fff92917c0d74b2950a72dc3243be5ade9b
kimjinsoo <roorooro9933@gmail.com>
2021-04-05 21:46:15 +0900

    add mysql consulting-answer model

---
+++ server/models/mysql/consulting-answer.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require("sequelize");
+
+const sequelize = require("../../util/mysql");
+
+const Lawyer = require("./lawyer");
+const ConsultingQuestion = require("./consulting-question");
+
+const ConsultingAnswer = sequelize.define("consulting-answer", {
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  lawyerId: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    reference: {
+      model: Lawyer,
+      key: "id",
+    },
+    allowNull: false,
+  },
+  consultingQuestionId: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    reference: {
+      model: ConsultingQuestion,
+      key: "id",
+    },
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  recommendationCount: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    defaultValue: 0,
+  },
+});
+
+module.exports = ConsultingAnswer;
