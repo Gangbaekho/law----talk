a7bc1d43a141c60b34910ddd359fc4c11333fbbe
kimjinsoo <roorooro9933@gmail.com>
2021-04-06 21:36:58 +0900

    add mysql post table

---
+++ server/models/mysql/post.js
@@ -0,0 +1,54 @@
+const { DataTypes } = require("sequelize");
+
+const sequelize = require("../../util/mysql");
+const Lawyer = require("./lawyer");
+const SpecificDomain = require("./specific-domain");
+
+const postType = require("../../constants/postType");
+
+const Post = sequelize.defind("post", {
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  lawyerId: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    reference: {
+      model: Lawyer,
+      key: "id",
+    },
+    allowNull: false,
+  },
+  specificDomainId: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    reference: {
+      model: SpecificDomain,
+      key: "id",
+    },
+    allowNull: false,
+  },
+  postType: {
+    type: DataTypes.ENUM(postType),
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  postImageUrl: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  reviewCount: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    defaultValue: 0,
+  },
+});
+
+module.exports = Post;
