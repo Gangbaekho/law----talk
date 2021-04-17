51c4ceea82cd98bbf18e30ebf66f776dea1de771
kimjinsoo <roorooro9933@gmail.com>
2021-04-17 17:41:29 +0900

    add general domain, specific domain validation logic

--- server/graphql/resolvers/mysql/general-domain.js
+++ server/graphql/resolvers/mysql/general-domain.js
@@ -1,5 +1,11 @@
 const GeneralDomain = require("../../../models/mysql/general-domain");
 
+const yup = require("yup");
+
+const schema = yup.object().shape({
+  domainName: yup.string().min(1).required(),
+});
+
 const generalDomainResolver = {
   Query: {
     generalDomain: async (_, { id }) => {
@@ -9,6 +15,16 @@ const generalDomainResolver = {
   },
   Mutation: {
     createGeneralDomain: async (_, { domainName }) => {
+      await schema.validate({ domainName });
+
+      const doesExist = await GeneralDomain.findOne({ where: { domainName } });
+
+      if (doesExist) {
+        const error = new Error("general domain already exist");
+        error.statusCode = 422;
+        throw error;
+      }
+
       const generalDomain = await GeneralDomain.create({ domainName });
       return generalDomain.id;
     },
--- server/graphql/resolvers/mysql/specific-domain.js
+++ server/graphql/resolvers/mysql/specific-domain.js
@@ -1,4 +1,9 @@
 const SpecificDomain = require("../../../models/mysql/specific-domain");
+const yup = require("yup");
+
+const schema = yup.object().shape({
+  domainName: yup.string().min(1).required(),
+});
 
 const SpecificDomainResolver = {
   Query: {
@@ -9,6 +14,16 @@ const SpecificDomainResolver = {
   },
   Mutation: {
     createSpecificDomain: async (_, { generalDomainId, domainName }) => {
+      await schema.validate({ domainName });
+
+      const doesExist = await SpecificDomain.findOne({ where: { domainName } });
+
+      if (doesExist) {
+        const error = new Error("specific domain already exist");
+        error.statusCode = 422;
+        throw error;
+      }
+
       const specificDomain = await SpecificDomain.create({
         generalDomainId,
         domainName,
