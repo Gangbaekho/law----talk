91d9a11140afe18600242d82417efa4d6e79f686
kimjinsoo <roorooro9933@gmail.com>
2021-04-21 18:42:42 +0900

    setting connection pool to sequelize

--- server/util/mysql.js
+++ server/util/mysql.js
@@ -3,6 +3,12 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("lawtalk", "jinsoo", "jinsoo", {
   dialect: "mysql",
   host: "localhost",
+  pool: {
+    max: 10,
+    min: 1,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 module.exports = sequelize;
