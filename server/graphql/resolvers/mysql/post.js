ded9a7521af09144a2234a28622f2f066a7e0a43
kimjinsoo <roorooro9933@gmail.com>
2021-04-22 22:55:39 +0900

    add transaction to post resolver

--- server/graphql/resolvers/mysql/post.js
+++ server/graphql/resolvers/mysql/post.js
@@ -1,6 +1,3 @@
-const Post = require("../../../models/mysql/post");
-const MongoLawyer = require("../../../models/mongo/lawyer");
-
 const POSTS_PER_PAGE = 10;
 
 const yup = require("yup");
@@ -13,22 +10,30 @@ const schema = yup.object().shape({
 
 const postResolver = {
   Query: {
-    post: async (_, { id }) => {
-      const post = await Post.findOne({ id });
-      return post.id;
-    },
-    getPosts: async (_, { specificDomainId, offset }) => {
-      const posts = await Post.findAll({
-        where: { specificDomainId },
-        offset,
-        limit: POSTS_PER_PAGE,
+    post: async (_, { id }, { models, transaction }) => {
+      return await transaction.repeatableReadTransaction(async () => {
+        const post = await models.Post.findOne({ id });
+        return post.id;
       });
+    },
+    getPosts: async (
+      _,
+      { specificDomainId, offset },
+      { models, transaction }
+    ) => {
+      return await transaction.repeatableReadTransaction(async () => {
+        const posts = await models.Post.findAll({
+          where: { specificDomainId },
+          offset,
+          limit: POSTS_PER_PAGE,
+        });
 
-      return posts;
+        return posts;
+      });
     },
   },
   Mutation: {
-    createPost: async (_, { postInput }) => {
+    createPost: async (_, { postInput }, { models, transaction }) => {
       const {
         lawyerId,
         mongoLawyerId,
@@ -41,17 +46,19 @@ const postResolver = {
 
       await schema.validate({ postType, title, content, postImageUrl });
 
-      const post = await Post.create({
-        lawyerId,
-        mongoLawyerId,
-        specificDomainId,
-        postType,
-        title,
-        content,
-        postImageUrl,
-      });
+      return await transaction.serializableTransaction(async () => {
+        const post = await Post.create({
+          lawyerId,
+          mongoLawyerId,
+          specificDomainId,
+          postType,
+          title,
+          content,
+          postImageUrl,
+        });
 
-      return post.id;
+        return post.id;
+      });
     },
   },
   Post: {
