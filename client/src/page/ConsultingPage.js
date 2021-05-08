b5baae0fb6bfe4ba550cf2a99643b7bd0f1f8e31
kimjinsoo <roorooro9933@gmail.com>
2021-05-08 20:24:19 +0900

    create ConsultingPage component blueprint

--- client/src/page/ConsultingPage.js
+++ client/src/page/ConsultingPage.js
@@ -1,15 +1,81 @@
 import React from "react";
 import MainHeader from "../component/MainHeader";
 import MainFooter from "../component/MainFooter";
+import styled from "styled-components";
+import Consulting from "../component/consulting/Consulting";
+import SideBanner from "../component/consulting/SideBanner";
 
 const ConsultingPage = (props) => {
   return (
     <>
       <MainHeader />
-      <div>content</div>
+      <StyleContainer>
+        <h2>상담사례</h2>
+        <div className="flex-container">
+          <div className="consultings">
+            <Consulting />
+            <Consulting />
+            <Consulting />
+            <Consulting />
+            <div className="pagination">
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+              </ul>
+            </div>
+          </div>
+          <div className="side">
+            <SideBanner />
+          </div>
+        </div>
+      </StyleContainer>
       <MainFooter />
     </>
   );
 };
+
+const StyleContainer = styled.div`
+  width: 1080px;
+  margin: 0 auto;
+
+  h2 {
+    margin: 2rem 0;
+    padding: 1rem 0;
+    border-bottom: 1px solid #555;
+    color: rgba(0, 0, 0, 0.8);
+  }
+
+  .flex-container {
+    display: flex;
+    .consultings {
+      width: 800px;
+      .pagination {
+        background-color: #f7f7f7;
+        height: 5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 4rem 0;
+        ul {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          list-style: none;
+          li {
+            margin: 0 1rem;
+            font-weight: bold;
+          }
+        }
+      }
+    }
+    .side {
+      width: 280px;
+    }
+  }
+`;
 
 export default ConsultingPage;
